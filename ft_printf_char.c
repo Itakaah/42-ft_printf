18d182231956f3c52215eb86876f9d6c27446a1d
Itakaah <am7.usmanov@gmail.com>
2025-11-28 12:32:47 +0100

    ft_print_char file for char conversion (%s, %c)

--- ft_printf_char.c
+++ ft_printf_char.c
@@ -1,13 +1,20 @@
 #include "ft_printf.h"
 
-int print_char(char c)
+int print_char(int c)
 {
-    // TODO
-    return (0);
+    ft_putchar_fd((char)c, 1);
+    return (1);
 }
 
 int print_string(char *s)
 {
-    // TODO
-    return (0);
+	int	s_len;
+
+    if (!s)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	s_len = ft_putstr_fd(s, 1);
+    return (s_len);
 }
